8dfb3ef9132cd7db63ea22b90a6641815c808b3e
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-15 12:53:11 +0200

    update

---
+++ Exercise Syntax Functions and Statements/sameNumbers.js
@@ -0,0 +1,17 @@
+function solve(num){
+    let sum=0;
+    let allIsSame=true;
+    num=num.toString();
+
+    for (let i = 0; i < num.length; i++) {
+        if (num[i]!==num[0]) {
+            allIsSame=false;
+        }
+        sum+= +num[i];
+    }
+
+    console.log(allIsSame);
+    console.log(sum);
+}
+
+solve(2222222)

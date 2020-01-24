c0ef7d5f2e7de176c64023a2b403f87797ec70fe
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-24 19:19:33 +0200

    update

---
+++ Lab Advanced Functions/currencyFormat.js
@@ -0,0 +1,22 @@
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+
+
+}
+
+function result(formater){
+    return function(val){
+        let sep=',';
+        let symb='$';
+        let symbFirst=true;
+        return formater(sep,symb,symbFirst,val)
+    };
+}
+
+let dollarFormatter = result(currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71

c47115655f4ca33f58aeb7e4a797c5a866b21ada
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-26 15:26:24 +0200

    update

---
+++ Exercise Advanced Functions/argumentInfo.js
@@ -0,0 +1,35 @@
+function solve(){
+    const elements={};
+
+    for (const element of arguments) {
+        let typeofElement=typeof(element);
+
+        console.log(`${typeofElement}: ${element}`)
+
+        if (!elements[typeofElement]) {
+            elements[typeofElement]=0;
+        }
+        elements[typeofElement]++;
+    }
+    Object.keys(elements).sort((a,b)=>elements[b]-elements[a]).forEach(key => {
+        console.log(`${key} = ${elements[key]}`);
+    });
+}
+
+
+solve({ name: 'bob'}, 3.333, 9.999);
+
+
+// var expectedOutput = [
+//     'object:',
+//     'number: 3.333',
+//     'number: 9.999',
+//     'number = 2',
+//     'string = 1'
+// ];
+
+// expect(output.length).to.equal(expectedOutput.length, "Incorrect number of arguments parsed.");
+// for (var i = 0; i < output.length; i++) {
+//     var current = output[i];
+//     expect(current).to.contains(expectedOutput[i], "Incorrect number of arguments parsed.");
+// }

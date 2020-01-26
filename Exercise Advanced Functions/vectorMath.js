d8a712f9c7b0349cb19a6950d862846c929c9258
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-26 15:27:01 +0200

    update

---
+++ Exercise Advanced Functions/vectorMath.js
@@ -0,0 +1,53 @@
+function result(){
+    add: function(vec1, vec2) {
+        let xa=vec1[0];
+        let ya=vec1[1];
+        let xb=vec2[0];
+        let yb=vec2[1];
+
+        return [xa+xb,ya+yb];
+    },
+
+    multiply: function(vec1, scalar){
+        let xa=vec1[0];
+        let ya=vec1[1];
+
+        return [xa*scalar,ya*scalar];
+    },
+
+    length: function(vec1){
+        let xa=vec1[0];
+        let ya=vec1[1];
+
+        return Math.sqrt(xa**2+ya**2,2);
+    },
+
+    dot: function(vec1, vec2){
+        let xa=vec1[0];
+        let ya=vec1[1];
+        let xb=vec2[0];
+        let yb=vec2[1];
+
+        return xa*ya+xb+yb;
+    },
+
+    cross: function(vec1, vec2){
+        let xa=vec1[0];
+        let ya=vec1[1];
+        let xb=vec2[0];
+        let yb=vec2[1];
+
+        return xa*yb-ya*xb;
+    }
+}
+
+var answer = result.add([1, 1], [1, 0]);
+var expected = [2, 1];
+var errorMessage = "Incorrect addition.";
+
+compareVectors(expected, answer);
+
+function compareVectors(vec1, vec2) {
+    expect(vec1[0]).to.equal(vec2[0], errorMessage);
+    expect(vec1[1]).to.equal(vec2[1], errorMessage);
+}

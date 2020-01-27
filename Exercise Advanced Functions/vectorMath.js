4692c957cd12eeac23fbcb533fc0016db75ec919
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-27 11:49:45 +0200

    update

--- Exercise Advanced Functions/vectorMath.js
+++ Exercise Advanced Functions/vectorMath.js
@@ -1,53 +1,44 @@
-function result(){
-    add: function(vec1, vec2) {
-        let xa=vec1[0];
-        let ya=vec1[1];
-        let xb=vec2[0];
-        let yb=vec2[1];
+function result() {
+        add= function (vec1, vec2) {
+            let xa = vec1[0];
+            let ya = vec1[1];
+            let xb = vec2[0];
+            let yb = vec2[1];
 
-        return [xa+xb,ya+yb];
-    },
+            return [xa + xb, ya + yb];
+        },
 
-    multiply: function(vec1, scalar){
-        let xa=vec1[0];
-        let ya=vec1[1];
+        multiply= function (vec1, scalar) {
+            let xa = vec1[0];
+            let ya = vec1[1];
 
-        return [xa*scalar,ya*scalar];
-    },
+            return [xa * scalar, ya * scalar];
+        },
 
-    length: function(vec1){
-        let xa=vec1[0];
-        let ya=vec1[1];
+        length= function (vec1) {
+            let xa = vec1[0];
+            let ya = vec1[1];
 
-        return Math.sqrt(xa**2+ya**2,2);
-    },
+            return Math.sqrt(xa ** 2 + ya ** 2, 2);
+        },
 
-    dot: function(vec1, vec2){
-        let xa=vec1[0];
-        let ya=vec1[1];
-        let xb=vec2[0];
-        let yb=vec2[1];
+        dot= function (vec1, vec2) {
+            let xa = vec1[0];
+            let ya = vec1[1];
+            let xb = vec2[0];
+            let yb = vec2[1];
 
-        return xa*ya+xb+yb;
-    },
+            return xa * ya + xb + yb;
+        },
 
-    cross: function(vec1, vec2){
-        let xa=vec1[0];
-        let ya=vec1[1];
-        let xb=vec2[0];
-        let yb=vec2[1];
+        cross= function (vec1, vec2) {
+            let xa = vec1[0];
+            let ya = vec1[1];
+            let xb = vec2[0];
+            let yb = vec2[1];
 
-        return xa*yb-ya*xb;
-    }
+            return xa * yb - ya * xb;
+        }
 }
 
 var answer = result.add([1, 1], [1, 0]);
-var expected = [2, 1];
-var errorMessage = "Incorrect addition.";
-
-compareVectors(expected, answer);
-
-function compareVectors(vec1, vec2) {
-    expect(vec1[0]).to.equal(vec2[0], errorMessage);
-    expect(vec1[1]).to.equal(vec2[1], errorMessage);
-}

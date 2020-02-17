cbcf39a2f93f9a161f2a7470623b3e75a3f9a8de
DraPpeRr <drapperr@users.noreply.github.com>
2020-02-17 09:50:21 +0200

    update

--- Exercise String and RegExp/02_Expedition-Problems/solution.js
+++ Exercise String and RegExp/02_Expedition-Problems/solution.js
@@ -5,27 +5,32 @@ function solve() {
 
     output.innerHTML = findCoordinates(string, text);
 
-    function findCoordinates(keyword, data) {
-        let regexCoordinate = /(north|east)[^\d]*(\d{2})[^,]*,[^,]*?(\d{6})/igm;
-                                    
-        let matches = data.match(regexCoordinate);
-        matches.reverse();
-        let northElements =/north[^\d]*(\d{2})[^,]*,[^,]*?(\d{6})/igm.exec(matches.find(x => x.toLowerCase().startsWith('north')));
-        let eastElements = /east[^\d]*(\d{2})[^,]*,[^,]*?(\d{6})/igm.exec(matches.find(x => x.toLowerCase().startsWith('east')));
-        
-        let north=northElements[1]+'.'+northElements[2];
-        let east=eastElements[1]+'.'+eastElements[2];
+    function findCoordinates(messageSign, text) {
+        let pattern = /(north|east)[\s\S]*?([0-9]{2})[^,]*?,[^,]*?([0-9]{6})/gim;
+        let mesagePattern = new RegExp(`${messageSign}([\\s\\S]*)${messageSign}`, 'gim');
     
-        let regexText = new RegExp(`${keyword}(.+)${keyword}`);
-        let text = data.match(regexText)[1];
+        let north = '';
+        let east = '';
+        
+        let currnetMatch = pattern.exec(text);
+        while (currnetMatch) {
+            if (currnetMatch[1].toLowerCase() === 'north') {
+                north = `${currnetMatch[2]}.${currnetMatch[3]} N`;
+            } else {
+                east = `${currnetMatch[2]}.${currnetMatch[3]} E`;
+            }
+    
+            currnetMatch = pattern.exec(text);
+        }
+    
+        let messageContent = 'Message: ' + mesagePattern.exec(text)[1];
     
         let result = [];
-        result.push(`<p>${north} N</p>`)
-        result.push(`<p>${east} E</p>`)
-        result.push(`<p>Message: ${text}</p>`)
-    
-        return result.join('');
+            result.push(`<p>${north}</p>`);
+            result.push(`<p>${east}</p>`);
+            result.push(`<p>${messageContent}</p>`);
+        
+            return result.join('');
     }
 }
 
-findCoordinates('<>','o u%&lu43t&^ftgv><nortH4276hrv756dcc,  jytbu64574655k <>ThE sanDwich is iN the refrIGErator<>yl i75evEAsTer23,lfwe 987324tlblu6b')

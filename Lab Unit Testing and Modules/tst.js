7594599cd47bda7650cbefd0dd7dc68de72c48b7
DraPpeRr <drapperr@users.noreply.github.com>
2020-02-21 12:53:19 +0200

    update

---
+++ Lab Unit Testing and Modules/tst.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+
+describe('Sum', function () {
+  it('should return 0 with zero elements', function () {
+    assert.equal(5, 5);
+  });
+  it('should return 10', function () {
+    assert.equal(3, 3);
+  });
+});

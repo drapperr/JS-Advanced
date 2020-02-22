d9197518986cc3c9a74e754f04a52c1f15bf8949
DraPpeRr <drapperr@users.noreply.github.com>
2020-02-22 14:26:56 +0200

    update

--- Exams/JS Advanced Retake Exam - 10 December 2019/Christmas Gifts Delivery/app.js
+++ Exams/JS Advanced Retake Exam - 10 December 2019/Christmas Gifts Delivery/app.js
@@ -1,62 +1,66 @@
 function solution() {
-    let addBtn = document.querySelector('.card div button');
-    let inputBox = document.querySelector('.card div input');
-    let listOfGifts = document.querySelectorAll('.card')[1].querySelector('ul');
-    let listOfSend = document.querySelectorAll('.card')[2].querySelector('ul');
-    let listOfDiscardeds = document.querySelectorAll('.card')[3].querySelector('ul');
+    const sections = document.querySelectorAll('section');
+    const input = sections[0].querySelector('input');
+    const addButton = sections[0].querySelector('button');
+    const listOfGifts = sections[1].querySelector('ul');
+    const listOfSends = sections[2].querySelector('ul');
+    const listOfDiscardeds = sections[3].querySelector('ul');
 
-    addBtn.addEventListener('click', addGift);
+    addButton.addEventListener('click', addGift);
 
     function addGift() {
-        let newLi = document.createElement('li');
-        newLi.classList.add('gift');
-        newLi.innerHTML = inputBox.value;
+        let newGift = createNewElent('li', input.value, null, 'gift');
+        let sendButton = createNewElent('button', 'Send', 'sendButton');
+        let discardButton = createNewElent('button', 'Discard', 'discardButton');
 
-        let sendBtn = document.createElement('button');
-        sendBtn.id = 'sendButton';
-        sendBtn.innerHTML = 'Send';
-        sendBtn.addEventListener('click', sendItem);
+        sendButton.addEventListener('click', sendGift);
+        discardButton.addEventListener('click', discardGift);
 
-        let discardBtn = document.createElement('button');
-        discardBtn.id = 'discardButton';
-        discardBtn.innerHTML = 'Discard';
-        discardBtn.addEventListener('click', discardItem);
+        addChildren(newGift, [sendButton, discardButton]);
+        listOfGifts.appendChild(newGift);
 
-        newLi.appendChild(sendBtn);
-        newLi.appendChild(discardBtn);
+        input.value = '';
 
-        listOfGifts.appendChild(newLi);
-
-        sortList();
-
-        inputBox.value = '';
-    }
-
-    function sendItem(e) {
-        let newLi = document.createElement('li');
-        newLi.classList.add('gift');
-        newLi.innerHTML = e.target.parentNode.innerHTML;
-        newLi.lastChild.remove();
-        newLi.lastChild.remove();
-        e.target.parentNode.remove();
-
-        listOfSend.append(newLi);
-    }
-
-    function discardItem(e) {
-        let newLi = document.createElement('li');
-        newLi.classList.add('gift');
-        newLi.innerHTML = e.target.parentNode.innerHTML;
-        newLi.lastChild.remove();
-        newLi.lastChild.remove();
-        e.target.parentNode.remove();
-
-        listOfDiscardeds.append(newLi);
-    }
-
-    function sortList() {
         const result = Array.from(listOfGifts.children).sort((a, b) => a.innerHTML.localeCompare(b.innerHTML));
         listOfGifts.innerHTML = '';
         result.map(li => listOfGifts.appendChild(li));
+    }
+
+    function sendGift(e) {
+        let currentGift = e.target.parentNode;
+        currentGift.children[0].remove();
+        currentGift.children[0].remove();
+        listOfSends.appendChild(currentGift)
+    }
+
+    function discardGift(e) {
+        let currentGift = e.target.parentNode;
+        currentGift.children[0].remove();
+        currentGift.children[0].remove();
+        listOfDiscardeds.appendChild(currentGift)
+    }
+
+    function createNewElent(name, text, id, clas) {
+        let el = document.createElement(name);
+
+        if (text) {
+            el.textContent = text;
+        }
+
+        if (id) {
+            el.id = id;
+        }
+
+        if (clas) {
+            el.classList.add(clas);
+        }
+
+        return el;
+    }
+
+    function addChildren(el, clidren) {
+        clidren.forEach(child => {
+            el.appendChild(child);
+        });
     }
 }

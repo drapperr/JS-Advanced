97bc9f5ce2d1add09e1f863a159b26a0f9b51bb0
DraPpeRr <drapperr@users.noreply.github.com>
2020-06-16 08:03:50 +0300

    update

---
+++ JS-Fundamentals/Arrays - Lab/Day of Week.js
@@ -0,0 +1,9 @@
+function solve(number){
+    let arr=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+    if (number>=1&&number<=7) {
+        console.log(arr[number-1]);
+    } else {
+        console.log('Invalid day!');
+    }
+}

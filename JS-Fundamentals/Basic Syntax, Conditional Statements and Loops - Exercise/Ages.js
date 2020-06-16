8cde448c136f8e95ae43fb675501a3a79ae8dad9
DraPpeRr <drapperr@users.noreply.github.com>
2020-06-16 08:04:33 +0300

    update

---
+++ JS-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/Ages.js
@@ -0,0 +1,17 @@
+function solve(age){
+    let result;
+
+    if (age>=0&&age<=2) {
+        result='baby';
+    } else if (age<=13) {
+        result='child';
+    } else if (age<=19) {
+        result='teenager';
+    } else if (age<=65) {
+        result='adult';
+    } else {
+        result='elder';
+    }
+
+    console.log(result);
+}

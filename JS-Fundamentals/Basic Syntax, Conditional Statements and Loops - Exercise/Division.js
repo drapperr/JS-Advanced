0bf5c68bf8f9ef9811ec6b2b74b8fbca60cdab47
DraPpeRr <drapperr@users.noreply.github.com>
2020-06-17 11:24:17 +0300

    update

---
+++ JS-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/Division.js
@@ -0,0 +1,21 @@
+function solve(number){
+    let divisible=0;
+
+    if (number%10==0) {
+        divisible=10;
+    } else if (number%7==0) {
+        divisible=7;
+    } else if (number%6==0) {
+        divisible=6;
+    } else if (number%3==0) {
+        divisible=3;
+    } else if (number%2==0) {
+        divisible=2;   
+    }
+
+    if (divisible==0) {
+        console.log('Not divisible')
+    } else{
+        console.log(`The number is divisible by ${divisible}`)
+    }
+}

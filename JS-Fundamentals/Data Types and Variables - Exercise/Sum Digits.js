37a2a74b43de82b3f0a9dfe2934d3f7fabd751d8
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-14 21:18:53 +0300

    update

---
+++ JS-Fundamentals/Data Types and Variables - Exercise/Sum Digits.js
@@ -0,0 +1,10 @@
+function solve(number){
+    number=number.toString();
+    let sum=0;
+
+    for (let i = 0; i < number.length; i++) {
+        sum+=Number(number[i]);
+    }
+
+    console.log(sum);
+}
